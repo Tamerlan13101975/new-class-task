27444440bc93e1295972b1db86f63bcb589aff94
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-03 17:49:16 +0300

    new task description

--- task.py
+++ task.py
@@ -6,3 +6,8 @@ class Task:
 
     def mark_as_done(self):
         self.status = True
+
+    def __str__(self):
+        return f"Описание задачи: {self.description}nСрок выполнения: {self.deadline}nСтатус: {'Выполнено' if self.status else 'Не выполнено'}"
+
+    
